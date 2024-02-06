63e12de7557674cd43dea1c68da2c7423013e5aa
AkioWm <cherif.kante16@gmail.com>
2024-02-06 14:48:04 +0100

    Create model Employee and add jakarta dependency

---
+++ src/main/java/com/openclassroom/api/model/Employee.java
@@ -0,0 +1,38 @@
+package com.openclassroom.api.model;
+
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    private String mail;
+
+    private String password;
+
+}
