4db582f5d6a9d1f77bffcc139337e3b28eff1b30
AkioWm <cherif.kante16@gmail.com>
2024-02-06 16:44:45 +0100

    Add controller, repo and service

---
+++ src/main/java/com/openclassroom/api/controller/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.openclassroom.api.controller;
+
+import com.openclassroom.api.model.Employee;
+import com.openclassroom.api.repository.EmployeeRepository;
+import com.openclassroom.api.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+    @GetMapping("/employees")
+    public Iterable<Employee> getEmployee(){
+        return employeeService.getEmployees();
+    }
+}
